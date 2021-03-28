ff566c6c9183be8c6811837c18308e173ef47c1c
Botxan <oihan.irca@gmail.com>
2021-03-28 21:32:37 +0200

    Added checkSensor class

---
+++ SmartFarm/src/packcheck/checkSensor.java
@@ -0,0 +1,17 @@
+package packcheck;
+import packfarm.Sensor;
+
+public class checkSensor {
+	public static void main(String[] args) {
+		Sensor sensor = new Sensor("ID452");
+		
+		System.out.println("Sensor: " + sensor.toString());
+		for (int i = 0; i < 10; i++) {
+			try {
+				System.out.println(sensor.collectValues().toString());	
+			} catch (Sensor.CollectErrorException e) {
+				System.out.println("Error while collecting physiological values");
+			}
+		}
+	}
+}
